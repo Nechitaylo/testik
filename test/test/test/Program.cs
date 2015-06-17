fc6f54cd4a41ed69092b5c4561ec353fd17b2102
Nechitaylo <dicoy@meta.ua>
2015-06-18 02:13:49 +0300

    first commit

---
+++ test/test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int a = 50;
+            int c;
+            c = a - i;
+            Console.WriteLine("c=" + c);
+            Console.ReadKey();
+        }
+    }
+}
