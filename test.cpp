035b45e74563b3def3942d448fca898a65bd0f25
Nechitaylo <dicoy@meta.ua>
2015-06-18 00:36:44 +0300

    init commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// test.cpp: определяет точку входа для консольного приложения.
+//
+#include <iostream>
+#include "stdafx.h"
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello/n";
+	return 0;
+}
+
